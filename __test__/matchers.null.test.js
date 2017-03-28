03f85bed2182daaa5db7674b5315ed34c9ab87c9
Lautaro Gruss <lgruss@madmobile.com>
2017-03-27 23:26:16 -0300

    initial commit

---
+++ __test__/matchers.null.test.js
@@ -0,0 +1,21 @@
+describe('my beverage', () => {
+ 
+  it('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+  it('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+
+});
